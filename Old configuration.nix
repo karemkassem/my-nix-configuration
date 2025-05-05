9d1a8f8f65307a45228303bb6f63b07b58166ae4
karemkassem <109902016+karemkassem@users.noreply.github.com>
2025-05-05 14:58:55 +0300

    Add files via upload

--- Old configuration.nix
+++ Old configuration.nix
@@ -1,22 +1,18 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, lib, ... }:
 
 {
   imports =
-    [
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # test for improving some networking problems
-#   networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
-
-
-
-  # sets the kernel to the latest kernel available
-#   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # enable bluetooth
   hardware.bluetooth.enable = true; # enables support for Bluetooth
@@ -26,80 +22,91 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; # enable nix flakes
 
 
+
   # needed drivers for opengl and intel ( mainly for hardware acceleration on firefox )
   hardware.graphics = {
     enable = true;
+
     extraPackages = with pkgs;[  
       mesa
       intel-media-driver 
+
       intel-vaapi-driver
       libvdpau-va-gl
       intel-compute-runtime
+
     ]; 
   };
-  
-  services = {
-    xserver = {
-      enable = true;
-      videoDrivers = [ "nvidia" ];
-    };
-    desktopManager.plasma6.enable = true;
-#     desktopManager.cosmic.enable = true;
-    displayManager = {
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-      };
-    };
-  };
+  # services.frigate.vaapiDriver = "iHD"; # Force intel vaapi driver
 
+  #  boot.blacklistedKernelModules = [ "nouveau" ];
+  
+
+#   nvidia drivers
   hardware.nvidia = {
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable; # Options : [ stable, latest, beta ]
+    # Modesetting is required.
+#     modesetting.enable = true;
 
-    videoAcceleration = true;
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
 
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false; ## change this to false if I finde buggy behaviour from the drivers
+
+    # Enable the Nvidia settings menu,
+	  # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    nvidiaPersistenced = true; # Might Improve HDMI connection when true
-
-    modesetting.enable = true;
-
-    dynamicBoost.enable = true; # might improve performance
-
-    open = true;
-
-
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.latest; # for the stable drivers : config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # laptop configuration
   hardware.nvidia.prime = {
+    # choose one of the two options only
+    # thers is another option c, but it's experimental, go to the wiki.
+		# Option A : offloading
+    # offload = {
+		# 	enable = true;
+		# 	enableOffloadCmd = true;
+		# };
+    # Option B : sync
     sync.enable = true;
-    # Make sure to use the correct Bus ID values for your system!
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
-  };
 
-  specialisation = {
-    on-the-go.configuration = {
-      system.nixos.tags = [ "on-the-go" ];
-      hardware.nvidia = {
-        prime.offload.enable = lib.mkForce true;
-        prime.offload.enableOffloadCmd = lib.mkForce true;
-        prime.sync.enable = lib.mkForce false;
-#         powerManagement = {
-#           enable = true;
-#           finegrained = true;
-#         };
-      };
-    };
-  };
+		# Make sure to use the correct Bus ID values for your system!
+		intelBusId = "PCI:0:2:0";
+		nvidiaBusId = "PCI:1:0:0";
+	};
+  # end of nvidia drivers
 
   # enable and default zsh
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
+  # sets the kernel to the latest kernel available 
+  # comment it to use the "lts" version
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -122,6 +129,60 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  ## enable Hyprland
+#   programs.hyprland.enable = true;
+
+  services = {
+    xserver = {
+      enable = true;
+      videoDrivers = [ "nvidia" ]; 
+    };
+    desktopManager.plasma6.enable = true;
+    displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+    };
+  };
+  # boot.kernelParams = [ "module_blacklist=i915" ]; # blacklist i915 for nvidia ## don't use this unlees ablsolutely necessary
+
+  ## disable nvidia card completely
+  # boot.extraModprobeConfig = ''
+  #   blacklist nouveau
+  #   options nouveau modeset=0
+  # '';   
+  # services.udev.extraRules = ''
+  #   # Remove NVIDIA USB xHCI Host Controller devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA USB Type-C UCSI devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA Audio devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA VGA/3D controller devices
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  # '';
+  # boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+
+  # spcial boot configuration for nvidia drivers, ( can add more options and more configurations )
+  # specialisation = {
+  #   nvidia.configuration = {
+  #     system.nixos.tags = [ "nvidia" ];
+  #     services.xserver.videoDrivers = [ "i915" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+  #   };
+
+  # };
+
+  specialisation = {
+    on-the-go.configuration = {
+      system.nixos.tags = [ "on-the-go" ];
+      hardware.nvidia = {
+        prime.offload.enable = lib.mkForce true;
+        prime.offload.enableOffloadCmd = lib.mkForce true;
+        prime.sync.enable = lib.mkForce false;
+      };
+    };
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -129,9 +190,13 @@
     variant = "";
   };
 
-  hardware.xpadneo.enable = true;
-#   hardware.xone.enable = true;
-
+  # # Enable CUPS to print documents.
+  # services.printing.enable = true;
+  # services.avahi = {
+  #   enable = true;
+  #   nssmdns4 = true;
+  #   openFirewall = true;
+  # };
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false; # switch to it if a flatpak app is lagging
@@ -142,6 +207,12 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = false;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    # media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -164,22 +235,41 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # to allow connections with ios
+  # services.usbmuxd.enable = true;
+
   # # xdg portal configuration
   xdg.portal = {
     enable = true;
+#     extraPortals = with pkgs; [ # highly likely that all this is unnecissary in kde plasma
+#       xdg-desktop-portal-hyprland
+#     ];
   };
+
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-
+    
     ## system packages 
     vulkan-loader
     vulkan-tools
     vulkan-validation-layers
     mesa-demos
-    bluez
-    bluez-tools
+    # libimobiledevice # to connect with ios
+
+    ## packages for hyprland
+#     lxqt.lxqt-policykit
+#     rofi-wayland
+#     # dunst ## disabled it for kde notifications
+#     libnotify # for dunst
+#     waybar
+#     swww
+#     pwvucontrol
+#     cliphist
+#     wl-clipboard
+#     brightnessctl
+#     libsForQt5.kservice
 
     ## terminal applications
     kitty
@@ -198,31 +288,33 @@
     kdePackages.filelight
     kdePackages.kclock
     vlc
+    resources
     onlyoffice-desktopeditors
-    kdePackages.klevernotes
     
     ## games
     protonup-qt
-    prismlauncher
+    gamemode
+    gamescope
+ 
 
 
-    ## browsers
-#     vivaldi-ffmpeg-codecs
-#     (vivaldi.overrideAttrs
-#       (oldAttrs: {
-#         dontWrapQtApps = false;
-#         dontPatchELF = true;
-#         nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-#           pkgs.kdePackages.wrapQtAppsHook
-#           pkgs.makeWrapper
-#         ];
-#         postFixup = oldAttrs.postFixup or "" + ''
-#           wrapProgram $out/bin/vivaldi \
-#             --add-flags "--enable-features=VaapiVideoDecodeLinuxGL" \
-#             --add-flags "--ignore-gpu-blocklist" \
-#             --add-flags "--enable-zero-copy"
-#         '';
-#       }))
+    ## chromium browsers
+    vivaldi-ffmpeg-codecs
+    (vivaldi.overrideAttrs
+      (oldAttrs: {
+        dontWrapQtApps = false;
+        dontPatchELF = true;
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+          pkgs.kdePackages.wrapQtAppsHook
+          pkgs.makeWrapper
+        ];
+        postFixup = oldAttrs.postFixup or "" + ''
+          wrapProgram $out/bin/vivaldi \
+            --add-flags "--enable-features=VaapiVideoDecodeLinuxGL" \
+            --add-flags "--ignore-gpu-blocklist" \
+            --add-flags "--enable-zero-copy"
+        '';
+      }))
 
 
 
@@ -231,10 +323,16 @@
     python3
     git
     vscode
-#     jetbrains.pycharm-professional
-#     zed-editor
+    jetbrains.pycharm-professional
+    zed-editor
 
     # language servers and packages for zed 
+    
+    # tree-sitter-grammars.tree-sitter-c
+    # tree-sitter-grammars.tree-sitter-markdown
+    # tree-sitter-grammars.tree-sitter-python
+    # tree-sitter-grammars.tree-sitter-java
+
     clang-tools # for clangd in c
 
 
@@ -553,15 +651,33 @@
   # Enable and download steam (NOTE : make sure to change the vulkan driver to nvidia)
   programs.steam = {
     enable = true;
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+  # enable gamemode for steam
+  programs.gamemode = {
+    enable = true;
+  # Optional settings:
+  # settings = {
+  #   general = {
+  #     renice = 10;
+  #   };
+  # };
+  };
+  
+
+
+
+  
 
   # enviromental variable that chooses the vulkan driver.
   # uncomment to use the intel drivers, if needed replace "intel" with "nvidia" to use nvidia drivers 
   # comment this to use nvidia drivers
-#   environment.variables.VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json;
+  environment.variables.VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json;
  
   ## Note: use DRI_PRIME=1 <application> if you want to run vulkan applications on nvidia
 
@@ -573,9 +689,32 @@
 
   # enable flatpak
   services.flatpak.enable = true;
-
-
   
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
